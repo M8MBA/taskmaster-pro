7adca0f2570b4678d965e26b3d31e2b399f26a4b
M8MBA <sauce.ix@gmail.com>
2022-02-18 22:07:26 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,7 +169,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -215,34 +215,45 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
-    console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
-    console.log("deactivate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function (event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
-  update: function (event) {
+  update: function () {
     // array to store the task data in
     var tempArr = [];
 
     // loop over current set of children in sortable list
     $(this).children().each(function () {
-      // trim down list's ID to match object property
-      var arrName = $(this)
-        .attr("id")
-        .replace("list-", "");
-
-      // update array on tasks object and save
-      tasks[arrName] = tempArr;
-      saveTasks();
+      // save values in temp array
+      tempArr.push({
+        text: $(this)
+          .find("p")
+          .text()
+          .trim(),
+        date: $(this)
+          .find("span")
+          .text()
+          .trim()
+      });
     });
+    // update array on tasks object and save
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
 
-    console.log(tempArr);
+    // update array on tasks object save
+    tasks[arrName] = tempArr;
+    saveTasks();
   }
 });
 
@@ -274,8 +285,8 @@ $("#remove-tasks").on("click", function () {
 // load tasks for the first time
 loadTasks();
 
-setInterval(function() {
-  $(".card .list-group-item").each(function(index, el) {
+setInterval(function () {
+  $(".card .list-group-item").each(function (index, el) {
     auditTask(el);
   });
 }, (1000 * 60) * 30);
